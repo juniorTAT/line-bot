831c095247eadf52a641cc5db0c1d59206593ddb
Anita Huang <a058742@gmail.com>
2021-04-12 14:31:52 +0800

    add sticker

--- app.py
+++ app.py
@@ -45,6 +45,7 @@ def handle_message(event):
     r = '很抱歉,你說甚麼?'
     if '貼圖' in msg:
         sticker_message = StickerSendMessage(package_id='1', sticker_id='1')
+        line_bot_api.reply_message(event.reply_token, sticker_message)
         return
     if msg in ['hi', 'Hi']:
         r = '嗨屁阿!'
